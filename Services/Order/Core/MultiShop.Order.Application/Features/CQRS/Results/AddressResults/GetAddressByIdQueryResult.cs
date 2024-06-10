1326057a548a425836170c333a8ee72a928be3b2
Sudenur Çelik <celikksudenur@gmail.com>
2024-06-11 00:59:06 +0300

    order-application

---
+++ Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Results/AddressResults/GetAddressByIdQueryResult.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Order.Application.Features.CQRS.Results.AddressResults
+{
+    //ID'ye göre adres getirme
+    public class GetAddressByIdQueryResult
+    {
+        public int AddressID { get; set; }
+        public string UserID { get; set; }
+        public string District { get; set; }
+        public string City { get; set; }
+        public string Detail { get; set; }
+
+    }
+}
---
+++ Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Results/AddressResults/GetAddressQueryResult.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Order.Application.Features.CQRS.Results.AddressResults
+{
+    //Adres sınıfında bulunan propertyler tutulur ve listelenmesi sağlanır.
+    public class GetAddressQueryResult
+    {
+        public int AddressID { get; set; }
+        public string UserID { get; set; }
+        public string District { get; set; }
+        public string City { get; set; }
+        public string Detail { get; set; }
+
+    }
+}
