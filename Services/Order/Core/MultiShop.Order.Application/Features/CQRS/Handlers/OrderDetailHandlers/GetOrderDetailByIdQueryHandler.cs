a955a7a222483eb81735ba76b9beba6799daef2c
Sudenur Çelik <celikksudenur@gmail.com>
2024-08-29 14:10:15 +0300

    Implemented CQRS design pattern

---
+++ Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Handlers/OrderDetailHandlers/GetOrderDetailByIdQueryHandler.cs
@@ -0,0 +1,38 @@
+﻿using MultiShop.Order.Application.Features.CQRS.Queries.AddressQueries;
+using MultiShop.Order.Application.Features.CQRS.Queries.OrderDetailQueries;
+using MultiShop.Order.Application.Features.CQRS.Results.AddressResults;
+using MultiShop.Order.Application.Features.CQRS.Results.OrderDetailResults;
+using MultiShop.Order.Application.Interfaces;
+using MultiShop.Order.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Order.Application.Features.CQRS.Handlers.OrderDetailHandlers
+{
+    public class GetOrderDetailByIdQueryHandler
+    {
+        private readonly IRepository<OrderDetail> _repository;
+
+        public GetOrderDetailByIdQueryHandler(IRepository<OrderDetail> repository)
+        {
+            _repository = repository;
+        }
+        public async Task<GetOrderDetailByIdQueryResult> Handle(GetOrderDetailByIdQuery query)
+        {
+            var values = await _repository.GetByIdAsync(query.Id);
+            return new GetOrderDetailByIdQueryResult
+            {
+                OrderDetailID = values.OrderDetailID,
+                ProductAmount = values.ProductAmount,
+                ProductID = values.ProductID,
+                ProductName = values.ProductName,
+                OrderingID = values.OrderingID,
+                ProductPrice = values.ProductPrice,
+                ProductTotalPrice = values.ProductTotalPrice
+            };
+        }
+    }
+}
--- Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Queries/OrderDetailQueries/GetOrderDetailByIdQuery.cs
+++ Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Queries/OrderDetailQueries/GetOrderDetailByIdQuery.cs
@@ -6,11 +6,11 @@ using System.Threading.Tasks;
 
 namespace MultiShop.Order.Application.Features.CQRS.Queries.OrderDetailQueries
 {
-    public class GetOrderDetailQuery
+    public class GetOrderDetailByIdQuery
     {
         public int Id { get; set; }
 
-        public GetOrderDetailQuery(int id)
+        public GetOrderDetailByIdQuery(int id)
         {
             Id = id;
         }
