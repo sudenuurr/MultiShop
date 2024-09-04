9c075a039e4afefad4950338bee0553dc5e133cd
Sudenur Çelik <celikksudenur@gmail.com>
2024-09-04 11:59:30 +0300

    Implemented DAL and Business Layer

---
+++ Services/Cargo/MultiShop.Cargo.DataAccessLayer/EntityFramework/EfCargoCompanyDal.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.SqlServer.Query.Internal;
+using MultiShop.Cargo.DataAccessLayer.Abstract;
+using MultiShop.Cargo.DataAccessLayer.Concrete;
+using MultiShop.Cargo.DataAccessLayer.Repositories;
+using MultiShop.Cargo.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Cargo.DataAccessLayer.EntityFramework
+{
+    public class EfCargoCompanyDal:GenericRepository<CargoCompany>,ICargoCompanyDal
+    {
+        public EfCargoCompanyDal(CargoContext context):base(context)
+        {
+            
+        }
+    }
+}
