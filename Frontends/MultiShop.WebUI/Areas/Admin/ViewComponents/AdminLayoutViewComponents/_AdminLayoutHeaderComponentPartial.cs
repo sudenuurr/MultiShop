3c22b8850eea5da7773430516fa644777b6f6dbc
Sudenur Çelik <celikksudenur@gmail.com>
2024-10-08 14:39:17 +0300

    Implemented admin dashboard

---
+++ Frontends/MultiShop.WebUI/Areas/Admin/ViewComponents/AdminLayoutViewComponents/_AdminLayoutHeaderComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.Areas.Admin.ViewComponents.AdminLayoutViewComponents
+{
+    public class _AdminLayoutHeaderComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+
+        return View(); 
+        }
+    }
+}
